24f02b705437425ed1675cb35b2c7b54b908373c
tebmik <lloyd.s.tom@gmail.com>
2020-05-20 11:21:31 +0100

    First-Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Simple back-end which contains access for...
+
+    users: email, password.
+
+    tracks: 
+    timestamp: Number,
+    coords: {
+        latitude: Number,
+        longitude: Number,
+        altitude: Number,
+        accuracy: Number,
+        heading: Number,
+        speed: Number
+    }
---
+++ src/index.js
@@ -0,0 +1,46 @@
+require('./models/User');
+require('./models/Track');
+const { MONGO_URI } = require("./config/config");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri = MONGO_URI;
+if (!mongoUri) {
+  throw new Error(
+    `MongoURI was not supplied.  Make sure you watch the video on setting up Mongo DB!`
+  );
+}
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', err => {
+  console.error('Error connecting to mongo', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+let port = process.env.PORT;
+
+if(port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
